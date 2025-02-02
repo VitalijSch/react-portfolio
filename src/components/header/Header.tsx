92d1925e56d2257a56cea2272384836c2b507645
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-02-02 12:59:21 +0100

    Create social-links and change css structure

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,44 +8,84 @@ import englishIcon from './../../assets/images/header/english.svg'
 import germanIcon from './../../assets/images/header/german.svg'
 
 function Header() {
-    const [isGermanLanguage, setIsGermanLanguage] = useState(false)
-
-    const toggleLanguage = () => setIsGermanLanguage(prev => !prev)
+    const [isGermanLanguage, setIsGermanLanguage] = useState<boolean>(false)
 
     return (
-        <header>
-            <div className="logo">
-                <span>V</span>
-                <span>S</span>
+        <header className="header">
+            <div className="header__logo">
+                <span className="header__logo-letter">V</span>
+                <span className="header__logo-letter">S</span>
             </div>
-            <div className="p-relative">
-                <img className="reveal-top hide-top" src={aboutMeIcon} alt="mark" />
-                <a>About me</a>
+
+            <div className="header__nav-item">
+                <img
+                    className="header__nav-icon header__reveal-top header__hide-top"
+                    src={aboutMeIcon}
+                    alt="mark"
+                />
+                <a className="header__nav-link">About me</a>
             </div>
-            <div className="p-relative">
-                <a>Skills</a>
-                <img className="reveal-left hide-left" src={skillsIcon} alt="underline" />
+
+            <div className="header__nav-item">
+                <a className="header__nav-link">Skills</a>
+                <img
+                    className="header__nav-icon header__reveal-left header__hide-left"
+                    src={skillsIcon}
+                    alt="underline"
+                />
             </div>
-            <div className="p-relative">
-                <a>Projects</a>
-                <img className="reveal-left hide-left" src={projectsIcon} alt="scrible" />
+
+            <div className="header__nav-item">
+                <a className="header__nav-link">Projects</a>
+                <img
+                    className="header__nav-icon header__reveal-left header__hide-left"
+                    src={projectsIcon}
+                    alt="scribble"
+                />
             </div>
-            <div className="p-relative">
-                <a>Contact</a>
-                <img className="reveal-left hide-left" src={contactIcon} alt="underline z" />
+
+            <div className="header__nav-item">
+                <a className="header__nav-link">Contact</a>
+                <img
+                    className="header__nav-icon header__reveal-left header__hide-left"
+                    src={contactIcon}
+                    alt="underline z"
+                />
             </div>
-            <div className="languages-container">
-                <div className="p-relative">
-                    <img onClick={toggleLanguage} className={`reveal-bottom-right hide-bottom-right ${isGermanLanguage ? "" : "animation-unset"}`}
-                        src={englishIcon} alt="border" />
-                    <span style={{ color: isGermanLanguage ? "#FFFFFF" : "#F87A55" }}>EN</span>
+
+            <div className="header__languages">
+                <div className="header__language-option  header__language-option--first">
+                    <img
+                        onClick={() => setIsGermanLanguage(false)}
+                        className={`header__language-icon header__reveal-bottom-right header__hide-bottom-right ${isGermanLanguage ? "" : "header__language-icon--no-animation"}`}
+                        src={englishIcon}
+                        alt="border"
+                    />
+                    <span
+                        className="header__language-label"
+                        style={{ color: isGermanLanguage ? "#FFFFFF" : "#F87A55" }}
+                    >
+                        EN
+                    </span>
                 </div>
-                <div className="slider">
-                    <div className={`circle ${isGermanLanguage ? "slider-animation" : ""}`}></div>
+
+                <div className="header__language-slider">
+                    <div className={`header__language-circle ${isGermanLanguage ? "header__language-circle--active" : ""}`}></div>
                 </div>
-                <div className="p-relative">
-                    <img onClick={toggleLanguage} className={`reveal-top-left hide-top-left ${isGermanLanguage ? "animation-unset" : ""}`} src={germanIcon} alt="border" />
-                    <span style={{ color: isGermanLanguage ? "#F87A55" : "#FFFFFF" }}>DE</span>
+
+                <div className="header__language-option header__language-option--second">
+                    <img
+                        onClick={() => setIsGermanLanguage(true)}
+                        className={`header__language-icon header__reveal-top-left header__hide-top-left ${isGermanLanguage ? "header__language-icon--no-animation" : ""}`}
+                        src={germanIcon}
+                        alt="border"
+                    />
+                    <span
+                        className="header__language-label"
+                        style={{ color: isGermanLanguage ? "#F87A55" : "#FFFFFF" }}
+                    >
+                        DE
+                    </span>
                 </div>
             </div>
         </header>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,6 +3,9 @@ import './Hero.css'
 import frameIcon from './../../assets/images/hero/picture.svg'
 import profileIcon from './../../assets/images/hero/profile.png'
 import wavingHandIcon from './../../assets/images/hero/waving-hand.svg'
+import linkedinIcon from './../../assets/images/hero/linkedin.svg'
+import githubIcon from './../../assets/images/hero/github.svg'
+import mailIcon from './../../assets/images/hero/mail.svg'
 import Header from '../header/Header'
 
 function Hero() {
@@ -39,54 +42,73 @@ function Hero() {
     return (
         <div className="hero">
             <Header />
-            <div className="hero__container">
-                <div className="hero__top">
-                    <div className="hero__left">
-                        <div
-                            className="button button--interactive"
-                            onMouseEnter={getNameDelayed}
-                            onMouseLeave={resetNameDelayed}
-                        >
-                            {getName}
-                            <img src={wavingHandIcon} alt="waving hand" className="button__icon button__icon--interactive" />
+            <div className="hero__main-container">
+                <div className="hero__container">
+                    <div className="hero__top">
+                        <div className="hero__left">
+                            <div
+                                className="hero__button hero__button--animation"
+                                onMouseEnter={getNameDelayed}
+                                onMouseLeave={resetNameDelayed}
+                            >
+                                {getName}
+                                <img src={wavingHandIcon} alt="waving hand" className="hero__button-icon" />
+                            </div>
+
+                            <div className="hero__headline">
+                                <span className="hero__headline-text hero__headline-text--lower">F</span>
+                                <span className="hero__headline-text hero__headline-text--upper">r</span>
+                                <span className="hero__headline-text hero__headline-text--upper">o</span>
+                                <span className="hero__headline-text hero__headline-text--upper">n</span>
+                                <span className="hero__headline-text hero__headline-text--upper">t</span>
+                                <span className="hero__headline-text hero__headline-text--upper">e</span>
+                                <span className="hero__headline-text hero__headline-text--upper">n</span>
+                                <span className="hero__headline-text hero__headline-text--upper">d</span>
+                            </div>
                         </div>
-                        <div className="hero__headline">
-                            <span className="hero__headline__text--interactive hero__headline__text--lower">F</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">r</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">o</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">n</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">t</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">e</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">n</span>
-                            <span className="hero__headline__text--interactive hero__headline__text--upper">d</span>
+
+                        <div
+                            className="hero__picture"
+                            onMouseEnter={getLaughingSmileyDelayed}
+                            onMouseLeave={resetLaughingSmileyDelayed}
+                        >
+                            <img src={frameIcon} alt="frame" className="hero__picture-frame" />
+                            <img src={profileIcon} alt="profile" className="hero__picture-profile" />
+                            <div className="hero__name">
+                                <span>Vitalij</span>
+                                <span>{getLaughingSmiley}</span>
+                            </div>
                         </div>
                     </div>
-                    <div
-                        className="hero__picture"
-                        onMouseEnter={getLaughingSmileyDelayed}
-                        onMouseLeave={resetLaughingSmileyDelayed}
-                    >
-                        <img src={frameIcon} alt="frame" className="hero__picture__frame" />
-                        <img src={profileIcon} alt="profile" className="hero__picture__profile hero__picture__profile--interactive" />
-                        <div className="hero__name">
-                            <span>Vitalij</span>
-                            <span>{getLaughingSmiley}</span>
+
+                    <div className="hero__bottom">
+                        <div className="hero__headline hero__bottom-headline">
+                            <span className="hero__headline-text hero__headline-text--lower">D</span>
+                            <span className="hero__headline-text hero__headline-text--lower">E</span>
+                            <span className="hero__headline-text hero__headline-text--lower">V</span>
+                            <span className="hero__headline-text hero__headline-text--lower">E</span>
+                            <span className="hero__headline-text hero__headline-text--lower">L</span>
+                            <span className="hero__headline-text hero__headline-text--lower">O</span>
+                            <span className="hero__headline-text hero__headline-text--lower">P</span>
+                            <span className="hero__headline-text hero__headline-text--lower">E</span>
+                            <span className="hero__headline-text hero__headline-text--lower">R</span>
                         </div>
+                        <span className="hero__button hero__bottom-button">Get in Touch</span>
                     </div>
                 </div>
-                <div className="hero__bottom">
-                    <div className="hero__headline hero__bottom__headline">
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">D</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">E</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">V</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">E</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">L</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">O</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">P</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">E</span>
-                        <span className="hero__headline__text--interactive hero__headline__text--lower">R</span>
+
+                <div className="hero__social-container">
+                    <div className="hero__social-link">
+                        <img src={linkedinIcon} alt="linkedin" />
                     </div>
-                    <span className="button hero__bottom__button hero__bottom__buttom--interactive">Get in Touch</span>
+
+                    <div className="hero__social-link">
+                        <img src={githubIcon} alt="github" />
+                    </div>
+
+                    <div className="hero__social-link">
+                        <img src={mailIcon} alt="mail" />
+                    </div>
                 </div>
             </div>
         </div>
